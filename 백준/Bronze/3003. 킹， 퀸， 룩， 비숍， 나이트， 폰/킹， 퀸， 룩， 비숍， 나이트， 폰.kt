3e03f41185feb0e4de2857827c9cf62ed2ee97e8
Jeong Ho <gh7052@gmail.com>
2023-01-13 18:29:50 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 124 ms, Memory: 13576 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val a = 1 - nextInt()
+    val b = 1 - nextInt()
+    val c = 2 - nextInt()
+    val d = 2 - nextInt()
+    val e = 2 - nextInt()
+    val f = 8 - nextInt()
+    
+    print("$a $b $c $d $e $f")
+}
