fa0b35655a164e64d2330fad10c60593915beb7d
Jeong Ho <gh7052@gmail.com>
2023-01-13 21:06:39 +0900

    [Bronze V] Title: 새싹, Time: 192 ms, Memory: 25880 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val a = """
+    .         ,r'"7
+    .r`-_   ,'  ,/
+    . \. ". L_r'
+    .   `~\/
+    .      |
+    .      |"""
+    
+    print(a.trimMargin("."))
+}
