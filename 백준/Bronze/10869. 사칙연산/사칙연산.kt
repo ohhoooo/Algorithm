709e9cea75c40b3dcee3650b4120a69cf753d691
Jeong Ho <gh7052@gmail.com>
2023-01-13 18:15:21 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 13524 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val a = nextInt()
+    val b = nextInt()
+    
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+}
