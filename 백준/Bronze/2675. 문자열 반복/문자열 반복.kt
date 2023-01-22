3b315a09cc37ad5c4d5366708c014e5e7e376b59
Jeong Ho <gh7052@gmail.com>
2023-01-22 23:48:06 +0900

    [Bronze II] Title: 문자열 반복, Time: 108 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val t = readLine().toInt()
+
+    repeat(t) {
+        val (re: String, str: String) = readLine()
+            .split(" ")
+        var result = ""
+
+        for (char in str) {
+            repeat(re.toInt()) {
+                result += char
+            }
+        }
+        println(result)
+    }
+}
