7655473f82cbb12e78968c7a5f1a265c99f6c455
Jeong Ho <gh7052@gmail.com>
2023-02-06 18:28:16 +0900

    [Silver V] Title: 색종이, Time: 196 ms, Memory: 22328 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.kt
@@ -0,0 +1,33 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val list = mutableListOf<Int>()
+    var x: Int
+    var y: Int
+    var size = 0
+    val num = readLine().toInt()
+    
+    if(num == 0) print(0)
+    else {
+        repeat(num) {
+        	val (xNum, yNum) = readLine().split(" ").map { it.toInt() }
+        	list.add(xNum)
+        	list.add(yNum)
+    	}
+        
+    	for(i in 0..99) {
+        	for(j in 0..99) {
+            	for(k in 0 until num*2 step 2) {
+                	x = list[k]
+                	y = list[k+1]
+                	if(j >= x && j < x+10 && i >= y && i < y+10) {
+                        size++
+                        break
+                    }
+            	}
+        	}
+    	}
+    	print(size)
+    }
+}
