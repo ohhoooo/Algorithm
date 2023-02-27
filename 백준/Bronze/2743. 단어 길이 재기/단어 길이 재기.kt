bdbceae255283f9042343aaa03e37c187325fa2d
Jeong Ho <gh7052@gmail.com>
2023-02-28 00:45:33 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 88 ms, Memory: 11920 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.kt
@@ -0,0 +1,8 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    print(readLine().length)
+}
