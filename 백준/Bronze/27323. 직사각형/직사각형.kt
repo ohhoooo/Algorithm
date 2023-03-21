0b331150bf16983639825b5e19fc8c5496baf350
Jeong Ho <gh7052@gmail.com>
2023-03-21 22:15:51 +0900

    [Bronze V] Title: 직사각형, Time: 92 ms, Memory: 12176 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.kt
@@ -0,0 +1,6 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    print(readLine().toInt() * readLine().toInt())
+}
