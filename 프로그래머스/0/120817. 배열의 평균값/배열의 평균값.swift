6f804580606c16756f6c132c2efc96c18f7e737a
Jeongho Kim <gh7052@gmail.com>
2024-01-24 16:05:42 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> Double {
+    
+    var sum = 0
+    
+    for number in numbers {
+        sum += number
+    }
+    
+    return Double(sum) / Double(numbers.count)
+}
