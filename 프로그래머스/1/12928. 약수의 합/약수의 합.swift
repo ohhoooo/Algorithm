1e9fc74ec74ba024a14f33356a074cc73ca99742
Jeongho Kim <gh7052@gmail.com>
2024-01-28 19:50:56 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.swift
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.swift
@@ -4,11 +4,13 @@ func solution(_ n:Int) -> Int {
     
     if n == 0 { return 0 } else if n == 1 { return 1 }
         
-    for i in 1...n {
+    for i in 1...n/2 {
         if n % i == 0 {
             answer += i
         }
     }
+    
+    answer += n
     
     return answer
 }
