01189f7cb4e481d24c95aa226181e5f332b667c8
Jeongho Kim <gh7052@gmail.com>
2024-02-20 16:24:45 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -0,0 +1,13 @@
+func solution(_ n:Int) -> String {
+    var answer = ""
+    
+    for i in 1...n {
+        if i % 2 == 1 {
+            answer += "수"
+        } else {
+            answer += "박"
+        }
+    }
+    
+    return answer
+}
