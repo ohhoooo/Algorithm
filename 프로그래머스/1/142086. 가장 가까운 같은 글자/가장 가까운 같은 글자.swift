ed3d64f1c7a3beceebfbd659e4c8cd6100ebacc9
Jeongho Kim <gh7052@gmail.com>
2024-02-29 11:34:03 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 34.48 ms, Memory: 17.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
@@ -0,0 +1,25 @@
+func solution(_ s:String) -> [Int] {
+    var answer: [Int] = []
+    let sArr = s.map { String($0) }
+    
+    for index in 0...sArr.count-1 {
+        if index == 0 {
+            answer.append(-1)
+        } else {
+            var count = 1
+            
+            for reverseIndex in stride(from: index-1, to: -1, by: -1) {
+                if sArr[index] == sArr[reverseIndex] {
+                    answer.append(count)
+                    break
+                } else if reverseIndex == 0 && sArr[index] != sArr[reverseIndex] {
+                    answer.append(-1)
+                } else {
+                    count += 1
+                }
+            }
+        }
+    }
+    
+    return answer
+}
