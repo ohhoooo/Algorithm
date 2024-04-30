994fc49635d9e9a9ea1bb1e9a1ca190affd043d7
Jeongho Kim <gh7052@gmail.com>
2024-04-30 12:10:34 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.swift
@@ -0,0 +1,33 @@
+func solution(_ ingredient:[Int]) -> Int {
+    var answer = 0
+    var ingredients = [Int]()
+    
+    for i in ingredient {
+        ingredients.append(i)
+        
+        if ingredients.count >= 4 {
+            if ingredients[ingredients.count-4...ingredients.count-1] == [1,2,3,1] {
+                // ingredients.remove(at: ingredients.count-1)
+                // ingredients.remove(at: ingredients.count-1)
+                // ingredients.remove(at: ingredients.count-1)
+                // ingredients.remove(at: ingredients.count-1)
+                
+                ingredients.removeSubrange(ingredients.count-4...ingredients.count-1)
+                
+                answer += 1
+            }
+//             if ingredients[ingredients.count-4] == 1 && ingredients[ingredients.count-3] == 2 && ingredients[ingredients.count-2] == 3 && ingredients[ingredients.count-1] == 1 {
+//                 ingredients.remove(at: ingredients.count-1)
+//                 ingredients.remove(at: ingredients.count-1)
+//                 ingredients.remove(at: ingredients.count-1)
+//                 ingredients.remove(at: ingredients.count-1)
+                
+//                 answer += 1
+//             }
+        }
+        
+        
+    }
+    
+    return answer
+}
